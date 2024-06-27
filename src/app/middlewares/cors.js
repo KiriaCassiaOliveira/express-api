0baca994a07b7e44390dca89d335b2ac499ec06b
Kíria Cássia de Oliveira <kiriacsoliveira@gmail.com>
2024-06-27 14:10:41 -0300

    feat: adjust url aws

--- src/app/middlewares/cors.js
+++ src/app/middlewares/cors.js
@@ -1,5 +1,5 @@
 module.exports = (request, response, next) => {
-  response.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  response.setHeader('Access-Control-Allow-Origin', 'http://my-contacts-deploy.s3-website-sa-east-1.amazonaws.com');
   response.setHeader('Access-Control-Allow-Methods', '*');
   response.setHeader('Access-Control-Allow-Headers', '*');
   response.setHeader('Access-Control-Max-Age', '10');
